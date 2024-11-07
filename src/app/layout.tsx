1966915f7fa507e86a7116953cff419b822ffd13
Karen Pirumyan <k.pirumyan@gmail.com>
2024-11-08 00:50:55 +0400

    Title theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import "./global.css";
 import { Footer } from "@/components/sections";
-import data from "@/data/metadata.json";
-import { Metadata } from "next";
 import React, { ReactNode, Suspense } from "react";
 import Loading from "./loading";
 import { jost } from "@/app/fonts";
-
-export const metadata: Metadata = data;
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`bg-secondary ${jost.className}`}>
         <header>{/*<Menu />*/}</header>
-        <main>{children}</main>
+        <main className="container">{children}</main>
         <footer>
           <section id="contacts">
             <Suspense fallback={<Loading />}>
