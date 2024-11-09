6ffafb447ad68ebd8ea70d55b7f08ce930018a68
Karen Pirumyan <k.pirumyan@gmail.com>
2024-11-09 23:02:57 +0400

    home decorations_3

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -2,11 +2,25 @@ import Info from "./Info";
 import Phrases from "./Phrases";
 import MainImage from "./MainImage";
 import { caveat } from "@/app/fonts";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-12 mt-8">
+        <div className="relative col-span-1">
+          <div className="v-line-block"></div>
+          <div className="relative w-[141px] h-full">
+            <Image
+              src="/pat-2.png"
+              alt="Image decoration"
+              width={141}
+              height={141}
+              className="absolute -bottom-[67px] left-[3px]"
+            />
+          </div>
+        </div>
+
         <div className="mt-24 col-start-2 col-span-6">
           <Info />
           <Phrases />
